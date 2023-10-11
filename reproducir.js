d53588ebc63029777858a925aebbb311a7e07ff2
angiearias1 <113912032+angiearias1@users.noreply.github.com>
2023-10-10 21:56:47 -0500

    Pagina principal

---
+++ reproducir.js
@@ -0,0 +1,8 @@
+
+window.addEventListener("DOMContentLoaded", () => {
+    const audio = document.createElement("audio");
+    audio.preload = "auto";
+    audio.src = "img/cancion.mp3";
+    audio.play();
+    document.body.appendChild(audio);
+});
