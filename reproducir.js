80bbf9f709df8bb3ebd684693974f37f43581fc3
angiearias1 <113912032+angiearias1@users.noreply.github.com>
2023-10-11 14:23:33 -0500

    Suena música principal

--- reproducir.js
+++ reproducir.js
@@ -1,9 +1,22 @@
 
-window.addEventListener("DOMContentLoaded", () => {
-    const audio = document.createElement("audio");
-    audio.preload = "auto";
-    audio.src = "img/cancion.mp3";
-    audio.play();
-    document.body.appendChild(audio);
-    muted="muted"
+function reproducirAudio(){
+const miMusica = document.getElementById("miAudio");
+const botonMusica = document.getElementById("my-button2");
+
+botonMusica.addEventListener("click", function() {
+  if (miMusica.paused) {
+    miMusica.play();
+    botonMusica.textContent = "Detener Música";
+  } else {
+    miMusica.pause();
+    botonMusica.textContent = "Reproducir Música";
+  }
 });
+
+}
+
+
+
+
+
+
